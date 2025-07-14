8910a74b56bb5acbd89221d2464e20249709ddf4
Adwait Ganguly <69320559+AGanguly13@users.noreply.github.com>
2025-07-14 11:55:52 -0400

    [RSDK-10920] migrate obstacles-distance from RDK to its own module (#3)

--- cmd/module/main.go
+++ cmd/module/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"obstaclesdistance"
 	"go.viam.com/rdk/module"
 	"go.viam.com/rdk/resource"
 	"go.viam.com/rdk/services/vision"
+	"obstaclesdistance"
 )
 
 func main() {
 	// ModularMain can take multiple APIModel arguments, if your module implements multiple models.
-	module.ModularMain(resource.APIModel{ vision.API, obstaclesdistance.ObstaclesDistance})
+	module.ModularMain(resource.APIModel{vision.API, obstaclesdistance.ObstaclesDistance})
 }
